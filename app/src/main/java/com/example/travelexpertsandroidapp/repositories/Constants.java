8dcb4567631df119168208c1820f263519024d72
Abhilash Paul <paul.abhilash@hotmail.com>
2019-10-25 09:47:41 -0600

    commititng the version presented

--- app/src/main/java/com/example/travelexpertsandroidapp/repositories/Constants.java
+++ app/src/main/java/com/example/travelexpertsandroidapp/repositories/Constants.java
@@ -1,7 +1,7 @@
 package com.example.travelexpertsandroidapp.repositories;
 
 public class Constants {
-    public static final String URL_TRAVELEXPERTS_SERVICE = "http://192.168.1.65:8181/TravelExperts/rs/";
+    public static final String URL_TRAVELEXPERTS_SERVICE = "http://10.187.251.205:8181/TravelExperts/rs/";
     public static final int PERMISSION_REQUEST_CODE = 200;
     public static final int DEFAULT_TRAVELLER_COUNT = 1;
 }
